5eec7ab7eee2d83fd660d8fe9c9aade65cf67db6
Edzisani <egithubprojects@gmail.com>
2021-10-26 16:16:08 +0200

    Latest changes to layout

--- app/src/main/java/com/example/thekittyapp/MainActivity.kt
+++ app/src/main/java/com/example/thekittyapp/MainActivity.kt
@@ -10,8 +10,6 @@ import com.example.thekittyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.binding = ActivityMainBinding.inflate(layoutInflater)
